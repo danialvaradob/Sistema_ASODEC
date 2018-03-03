15db475bba5da331afd2fb762fc503231a2b2d3c
danielalvarado <danialvarado@ic-itcr.ac.cr>
2018-03-03 11:58:47 -0600

    Print Receipt Missing

--- src/domain/Receipt.java
+++ src/domain/Receipt.java
@@ -5,10 +5,52 @@
  */
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Daniel Alvarado, Roberto Rojas
  */
 public class Receipt {
+    private List<ReceiptLine> receiptLines = new ArrayList<>();
+    public static int receiptID;
+    private boolean isPaid;
+    private float paidAmount;
+
+    public Receipt(boolean _isPaid, float _paidAmount) {
+        receiptID++;
+        this.isPaid = _isPaid;
+        this.paidAmount = _paidAmount;
+    }
+    
+    public void addLine(ReceiptLine _newLine) {
+        this.receiptLines.add(_newLine);
+    }
+    
+    public float getTotal() {
+        float total = 0;
+        int size = this.receiptLines.size();
+        int qty = 0;
+        for (int i = 0; i < size; i++) {
+            qty = this.receiptLines.get(i).getQuantity();
+            total += this.receiptLines.get(i).getProduct().getPrice() * qty;
+        
+        }
+        
+        return total;
+    }
+    
+    public String printReceipt() {
+        //todo
+        return " ";
+    
+    }
+    
+    
+    
+    
+    
+    
     
 }
